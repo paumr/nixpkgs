2b687cbec1353dea038b6f40ca9eee0cc8272ee6
paumr <paum@bdisk.eu>
2023-11-01 23:38:23 +0100

    archi: 5.1.0 -> 5.2.0

--- pkgs/tools/misc/archi/default.nix
+++ pkgs/tools/misc/archi/default.nix
@@ -12,23 +12,23 @@
 
 stdenv.mkDerivation rec {
   pname = "Archi";
-  version = "5.1.0";
+  version = "5.2.0";
 
   src =
     if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://www.archimatetool.com/downloads/archi-5.php?/${version}/Archi-Linux64-${version}.tgz";
-        sha256 = "1bv7xsjvxzr366zij1hy8mlsn6is3ykh1p50dkjxqg02f8k8cspm";
+        sha256 = "0c28gwpfq42mw2xc05q3x2wybwxzjx4ixkvch515dvy4bmdbhrdq";
       }
     else if stdenv.hostPlatform.system == "x86_64-darwin" then
       fetchzip {
         url = "https://www.archimatetool.com/downloads/archi-5.php?/${version}/Archi-Mac-${version}.dmg";
-        sha256 = "11z8xq3m0m8kpdwwldsjnhsf0wk7ybwsa5l9wfj215aj0dqk0w0y";
+        sha256 = "17xgynlvwp0mnzw7bykmjk64rig2pmrn6x1kfq8svfrh0qh5m3qq";
       }
     else if stdenv.hostPlatform.system == "aarch64-darwin" then
       fetchzip {
         url = "https://www.archimatetool.com/downloads/archi-5.php?/${version}/Archi-Mac-Silicon-${version}.dmg";
-        sha256 = "0ajsmnag1wn84kxczm3xnlyfycyy1hq6gswgvjl6xzlryjrigvf2";
+        sha256 = "1hfkj1snrbii5nx9mzzq2ngkj5bd2ys1gwb1w2k68f9nvnbss3r6";
       }
     else
       throw "Unsupported system";
